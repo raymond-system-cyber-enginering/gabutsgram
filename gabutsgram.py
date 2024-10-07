845e2fd923071581e8203c8daccb2c2f07c83441
raysysber <winxen.10388@ski.sch.id>
2024-10-07 22:47:52 +0700

    gabutsgram
    by shermond official

---
+++ gabutsgram.py
@@ -0,0 +1,73 @@
+#support shermond the owner!!
+#ini tools gabut doang mennnn!!!
+print("\033[32;1m======================[[gabutsgram))==========================")
+print("()----------------------------------------------------------()")
+print("()--organisasi-:-C.G.H.H.A----------------------------------()")
+print("()--version-:-1.0-------------------------------------------()")
+print("()--developer-:-shermond_official---------------------------()")
+print("()----------------------------------------------------------()")
+print("()--support-us-:--------------------------------------------()")
+print("()----------------------------------------------------------()")
+print("()--tiktok-:-tidak-tersedia---------------------------------()")
+print("()--instagram-:-yshrillbom_03-------------------------------()")
+print("()--whatsapp-:-6285141705599--------------------------------()")
+print("()----------------------------------------------------------()")
+print("()-------------------------(//CGHHA\\)-----------------------()")
+print("==============================================================")
+print("")
+print("welcome to gabutsgram silahkan pilih tools gabut dibawab :")
+print("")
+print("1.\033[33;1m hack wifi")
+print("")
+print("2.\033[33;1m hack satelit")
+print("")
+print("3.\033[33;1m keluar")
+print("")
+pilih = input("pilih : ")
+
+if pilih == "1":
+ wifin = input("masukan nama wifi : ")
+ print("(---")
+ print("(------")
+ print("(---------++-+(())---$/$--Be#/+###%$&&@&@---ms+#$/)))")
+ print("(------------ $$$$$$$$$$$-+((((((()))))))j?$+$++#$%%-")
+ print("(---------**++-%-$$$$$$$/(((@)((@)009200900$;;;///$()")
+ print("(------")
+ print("(---")
+ print("------------------wifi " + wifin + " sukses di hack!!")
+
+elif pilih == "3":
+ print("cara keluar CTRL +Z")
+
+elif pilih == "2":
+ print("pilih satelit :")
+ print("")
+ print("1.satelit amerika")
+ print("")
+ print("2.satelit israel")
+ print("")
+ print("3.satelit korea")
+ input = input("pilih satelit : ")
+else:
+ print("input yang anda masukkan salah!!!")
+
+if input == "1":
+ print("===================================")
+ print("-+++-++--++--++--++--++--++--++--++")
+ print("satelit amerika telah di hack!!")
+ print("")
+ print("-$+-++-++-+-+-#--&$&&&&&$+++#+-#-@-+")
+
+if input == "2":
+ print("===================================")
+ print("-++--++-++-+-+-+-+-+-+-+*+*+-++----")
+ print("satelit israel telah di hack!!")
+ print("") 
+ print("--$+$*+@@#&&#&#@+-#$*-$#*$&$&#%@$$s")
+
+elif input == "3":
+ print("===================================")
+ print("-+++-++--++--++--++--++--++--++--++")
+ print("satelit korea telah di hack!!")
+ print("")
+ print("-$+-++-++-+-+-#--&$&&&&&$+++#+-#-@-+")
